1325a03a38c14f20e898e168a7ca628cf55b72be
Ghazaly <gazaly@gmail.com>
2010-10-16 12:38:02 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offbudget::Application.config.secret_token = '38f784de5befcdf19d295210e076a2b7c0ac97fa80c5e314d2cfb3067c7c5dea15b0c9f3224908b0102ca8ae35645fbee175b9b86cc5d02f7212f4d275113b1d'
