b704e3b55bbd918c4a2ae9c3b1f696c51b528553
Ghazaly <gazaly@gmail.com>
2010-10-16 17:37:00 +0800

    finished designing index and other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Offbudget::Application.routes.draw do
   
   get 'pages/home'
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
   devise_for :users
 
   root :to => 'pages#index'
