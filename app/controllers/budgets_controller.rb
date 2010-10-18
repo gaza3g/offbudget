37fe202ad8927da5e60b38cac2f4d340b3f03a1d
Ghazaly <gazaly@gmail.com>
2010-10-18 23:57:07 +0800

    finished adding ajax support for create and delete actions in Budget

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,4 +1,5 @@
 class BudgetsController < ApplicationController
+  
   def index
     @budget = Budget.new if user_signed_in?
   end
@@ -6,20 +7,31 @@ class BudgetsController < ApplicationController
   def create
     name = params[:budget][:name]
     @budget  = current_user.budgets.build(params[:budget])
-    if @budget.save
-      flash[:success] = "#{name} created!"
-      redirect_to budgets_url
-    else
-      render budgets_url
+    respond_to do |format|
+      if @budget.save
+        flash[:success] = "#{name} created!"
+        #redirect_to budgets_url
+        format.html { redirect_to budgets_url }
+        format.js
+      else
+        #render budgets_url
+        format.html { render budgets_url}
+      end
     end
-    
   end
 
   def destroy
     name = Budget.find(params[:id]).name
-    Budget.find(params[:id]).destroy
-    flash[:success] = "#{name} deleted."
-    redirect_to budgets_url
+    respond_to do |format|
+      if Budget.find(params[:id]).destroy
+        flash[:success] = "#{name} deleted."
+        format.html { redirect_to budgets_url }
+        format.js
+      else
+        format.html {render budgets_url}
+      end
+    end
+    #redirect_to budgets_path
   end
   
 end
